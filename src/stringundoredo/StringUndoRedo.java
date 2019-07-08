120d470586cdadf29aa8156098797e866ace9627
Abdo-najjar <raf7awe.overlord@gmail.com>
2019-07-08 20:51:31 +0300

    :V

---
+++ src/stringundoredo/StringUndoRedo.java
@@ -0,0 +1,18 @@
+package stringundoredo;
+
+/**
+ *
+ * @author user
+ */
+public class StringUndoRedo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame f = new MainFrame();
+        f.setVisible(true);
+       
+    }
+    
+}
